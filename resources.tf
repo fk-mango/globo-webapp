f8e36fafda7078e3afa8abcbf1ae53c8ab7f00de
Francis <francis.mangornong@gmail.com>
2025-04-21 11:06:43 -0400

    add secrets manager

--- resources.tf
+++ resources.tf
@@ -28,9 +28,10 @@ locals {
 resource "aws_instance" "main" {
   count = length(data.tfe_outputs.networking.nonsensitive_values.public_subnet_ids)
 
-  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = var.instance_type
-  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnet_ids[count.index]
+  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  iam_instance_profile = aws_iam_instance_profile.webapp_instance_profile.name
+  instance_type        = var.instance_type
+  subnet_id            = data.tfe_outputs.networking.nonsensitive_values.public_subnet_ids[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -47,35 +48,22 @@ resource "aws_instance" "main" {
 
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
+    secret_id           = var.api_key_secret_id
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 
 }
 
-resource "terraform_data" "webapp" {
+resource "aws_iam_instance_profile" "webapp_instance_profile" {
+  name = "${local.name_prefix}-webapp-instance-profile"
 
-  triggers_replace = [
-    length(aws_instance.main.*.id),
-    join(",", aws_instance.main.*.id)
-  ]
+  role = var.ec2_role_name
 
-  provisioner "file" {
-    content = templatefile("./templates/application.config.tpl", {
-      hosts     = aws_instance.main.*.private_dns
-      site_name = "${local.name_prefix}-taco-wagon"
-      api_key   = var.api_key
-    })
-    destination = "/home/ec2-user/application.config"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    port        = "22"
-    host        = aws_instance.main[0].public_ip
-    private_key = module.ssh_keys.private_key_openssh
-  }
-
+  tags = local.common_tags
 }
+
+
 
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-webapp"
